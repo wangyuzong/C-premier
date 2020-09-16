7f12c14aa9f5147429a4be0d1cdf61640c9c0e31
Wangyuzong <1048521660@qq.com>
2020-09-16 08:44:56 +0800

    new delete reviwe

--- 第11章_使用类/运算符重载实例/重载矢量/vector.cpp
+++ 第11章_使用类/运算符重载实例/重载矢量/vector.cpp
@@ -41,40 +41,128 @@ namespace VECTOR
 		y = mag * sin(ang);
 	}
 	
-//public methods
-Vetor::Vector()     // default constructor
-{
-	x = y = mag = ang = 0.0;
-	mode = RECT;
-}
-
-// construct vector from rectangular coordinates if from is r
-// (the default) or else from polar coordinates if form is p
-Vector::Vector(double n1, double n2, Mode form)
-{
-	mode = form;
-	if(form == RECT)
+	//public methods
+	Vetor::Vector()     // default constructor
 	{
-		x = n1;
-		y = n2;
-		set_mag();
-		set_ang();
-	}
-	else if(form == POL)
-	{
-		mag = n1;
-		ang = n2 / Rad_to_deg;
-		set_x();
-		set_y();
-	}
-	else
-	{
-		cout << "Incorrect 3rd argument to Vector() -- ";
-		cout << "Vector set to 0\n";
 		x = y = mag = ang = 0.0;
 		mode = RECT;
 	}
-}
 
-// reset
-}
+	// construct vector from rectangular coordinates if from is r
+	// (the default) or else from polar coordinates if form is p
+	Vector::Vector(double n1, double n2, Mode form)
+	{
+		mode = form;
+		if(form == RECT)
+		{
+			x = n1;
+			y = n2;
+			set_mag();
+			set_ang();
+		}
+		else if(form == POL)
+		{
+			mag = n1;
+			ang = n2 / Rad_to_deg;
+			set_x();
+			set_y();
+		}
+		else
+		{
+			cout << "Incorrect 3rd argument to Vector() -- ";
+			cout << "Vector set to 0\n";
+			x = y = mag = ang = 0.0;
+			mode = RECT;
+		}
+	}
+
+	// reset vector from rectangular coordinates if form is
+	// RECT (the default) or else from polar coordinates if 
+	// form is POL
+	void vector::reset(double n1, double n2, Mode form)
+	{
+		mode = form;
+		if(form == RECT)
+		{
+			x = n1;
+			y = n2;
+			set_mag();
+			set_ang();
+		}
+		else if(form == POL)
+		{
+			mag = n1;
+			ang = n2 / Rad_to_deg;
+			set_x();
+			set_y();
+		}
+		else
+		{
+			cout << "Incorrect 3rd argument to Vector() -- ";
+			cout << "vector set to 0\n";
+			x = y = mag = ang = 0.0;
+			mode = RECT;
+		}
+	}
+
+	Vector::~Vector()   // destructor
+	{}
+
+	void Vector::polar_mode()    // set to polar mode
+	{
+		mode = POL;
+	}
+
+	void Vector::rect_mode()    // set to rectangular mode
+	{
+		mode = RECT;
+	}
+
+	// operator overloading
+	// add two Vectors
+	Vector Vector::operator+(const Vector& b) const
+	{
+		return Vector(x + b.x, y + b.y);
+	}
+
+	// subtract Vector b from a
+	Vector Vector::operator-(const Vector& b) const 
+	{
+		return Vector(x - b.x, y - b.y);
+	}
+
+	// reverse sign of Vector
+	Vector Vector::operator-() const 
+	{
+		return Vector(-x, -y);
+	}
+
+	// multiply vector by n
+	Vector Vector::operator*(double n) const
+	{
+		return Vector(n * x, n * y);
+	}
+
+	// friend methods
+	// multiply n by Vector a
+	Vector operator*(double n, const Vector& a)
+	{
+		return a * n;
+	}
+
+	// display rectangular coordinates if mode is RECT,
+	// else display polar coordinates if mode is POL
+	std::ostream& operator<<(std::ostream& os, const Vector& v)
+	{
+		if(v.mode == Vector::RECT)
+			os << "(x, y) = (" << v.x << "," << v.y << ")";
+		else if (v.mode == Vector::POL)
+		{
+			os << "(m, a) = (" << v.mag << ","
+				<< v.ang * Rad_to_deg << ")";
+		}
+		else
+			os << "Vector object mode is invalid";
+		return os;
+	}
+} // end namespace VECTOR
